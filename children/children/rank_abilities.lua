4f6c2782f89eb37600de017e02f8ffbce25948ba
muta69 <90420703+muta69@users.noreply.github.com>
2022-03-13 12:39:21 -0500

    Create rank_abilities.lua

---
+++ children/children/rank_abilities.lua
@@ -0,0 +1,91 @@
+local module = {
+	[1] = nil,
+	
+	[2] = nil,
+	
+	[3] = {
+		["give"] = true,
+		["respawn"] = false,
+		["refresh"] = false,
+		["kick"] = false
+	},
+	
+	[4] = {
+		["give"] = true,
+		["respawn"] = false,
+		["refresh"] = false,
+		["kick"] = false
+	},
+	
+	[5] = {
+		["give"] = true,
+		["respawn"] = false,
+		["refresh"] = false,
+		["kick"] = false
+	},
+	
+	[6] = {
+		["give"] = true,
+		["respawn"] = false,
+		["refresh"] = false,
+		["kick"] = false
+	},
+	
+	[7] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[8] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[9] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[10] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[11] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[253] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[254] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	},
+	
+	[255] = {
+		["give"] = true,
+		["respawn"] = true,
+		["refresh"] = true,
+		["kick"] = true
+	}
+}
+
+return module
